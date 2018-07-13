775f67c1802d6adaa918f52520307958e7961e8d
araudz <araudzens@gmail.com>
2018-07-13 16:25:35 -0600

    Adjusted plot and font sizes; Added separate difference panel

--- PlotFunctions/RPlot8.R
+++ PlotFunctions/RPlot8.R
@@ -1,8 +1,10 @@
 ### plot 8: total pressure (static + dynamic)
 RPlot8 <- function (data, ...) { 
-  op <- par (mar=c(5,4,1,1)+0.1, oma=c(1.1,0,0,0))
-  layout(matrix(1:1, ncol = 1), widths = 1, heights = 5)
-  DFP <- data.frame(Time=data$Time)
+  layout(matrix(1:3, ncol = 1), widths = 1)
+  op <- par (mar=c(5,5,5,1),oma=c(2,2,2,1))
+  par(cex.lab=2, cex.main=2)
+
+    DFP <- data.frame(Time=data$Time)
   ## find possible pairs
   if ('PS_A' %in% VRPlot[[8]]) {
     pair <- c(which ('PS_A' == VRPlot[[8]]), which ('QC_A' == VRPlot[[8]]))
@@ -21,9 +23,9 @@ RPlot8 <- function (data, ...) {
       DFP$PST3 <- data[, VRPlot[[8]][pair[1]]] + data[, VRPlot[[8]][pair[2]]]
     }
   }
-  DFP$Diff <- 20 * (DFP$PST1 - DFP$PST2) + 500
+  DFP$Diff <-  (DFP$PST1 - DFP$PST2) 
   if (length (VRPlot[[8]]) > 4) {
-    DFP$Diff2 <- 20*(DFP$PST1-DFP$PST3)+500
+    DFP$Diff2 <-(DFP$PST1-DFP$PST3)
   }
   # if ('PC' %in% names (data)) {
   #   DFP$PC <- 500 + 20 * (data$PSF * PCorFunction 
@@ -35,16 +37,23 @@ RPlot8 <- function (data, ...) {
     colnames <- c("Time", "PtotAvionics", "PtotF", "Diff1")
   }
   names(DFP) <- colnames
-  # colnames(DF) <- c("Time", "PtotF", "PtotAvionics", "Diff*20+500", 'PCOR chk')
-  plotWAC (DFP, col=c('blue', 'darkgreen', 'red', 'cyan', 'darkorange'), ylab='Ptot [hPa]',
+   # colnames(DF) <- c("Time", "PtotF", "PtotAvionics", "Diff*20+500", 'PCOR chk')
+  
+  
+  plotWAC (DFP[,colnames[!grepl('Diff',colnames)]], ylab='Total Pressures [hPa]',
            legend.position='topright')
-  abline(h=520, col='red', lty=2); abline(h=480, col='red', lty=2)
+  title('Total Pressures')
+  
+  #
+  plotWAC(DFP[,c("Time", colnames[grepl('Diff',colnames)])], 
+          ylim=c(-2,2), ylab='Differences [hPa]')
+  abline(h=-1, col='red', lty=2); abline(h=1, col='red', lty=2)
   if (length (VRPlot[[8]]) > 4) {
-    title (sprintf ("mean differences avionics - research 1 & 2: %.2f %.2f",
-      (mean (DFP$Diff1, na.rm=TRUE)-500)/20, (mean (DFP$Diff2, na.rm=TRUE)-500)/20), cex.main=0.75)
+     title (sprintf ("Mean Differences: avionics - research [1, 2]: [%.2f, %.2f]",
+      (mean (DFP$Diff1, na.rm=TRUE)-500)/20, (mean (DFP$Diff2, na.rm=TRUE)-500)/20))
   } else {
-    title (sprintf ("mean difference avionics-research: %.2f", 
-                  (mean (DFP$Diff1, na.rm=TRUE)-500)/20), cex.main=0.75)
+    title (sprintf ("Mean Difference: avionics-research: %.2f", 
+                  (mean (DFP$Diff1, na.rm=TRUE)-500)/20))
   }
   hline (0.2); hline (-0.2)
   AddFooter ()
