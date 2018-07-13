15d627e79d3c8721b163fede4c45ec817fd18d06
araudz <araudzens@gmail.com>
2018-07-13 12:54:17 -0600

    Updated radiation panels/variables; brush not working

--- PlotFunctions/RPlot14.R
+++ PlotFunctions/RPlot14.R
@@ -6,20 +6,34 @@ RPlot14 <- function(data, ...) {
   # layout(matrix(1:1, ncol = 1), widths = 1, heights = c(5,6))
   # op <- par (mar=c(5,4,1,2.5)+0.1,oma=c(1.1,0,0,0))
 
-  layout(matrix(1:3, ncol = 1), widths = 1, heights = c(5,5,6))
-  op <- par (mar=c(2,4,0.5,1)+0.1,oma=c(1.1,0,0,0))
+  layout(matrix(1:4, ncol = 1), widths = 1)
+  op <- par (mar=c(5,5,5,1),oma=c(2,2,2,1))
+  par(cex.lab=2, cex.main=2)
+  
+# Panel 1: RSTB
+  ylb <- expression (paste ("Temperature  [", degree, "C]")) 
   if ("RSTB" %in% VRPlot[[14]]) {
-    plotWAC (data[, c("Time", "RSTB")])
+    if ("RSTB1" %in% VRPlot[[14]]) {
+      plotWAC (data[, c("Time", "RSTB", "RSTB1")], ylab=ylb)
+    } else {
+      plotWAC (data[, c("Time", "RSTB")], ylab=ylb)
+    }
+    # title()
   }
+  
+# Panel 2: RSTT
+  if ("RSTT" %in% VRPlot[[14]]) {
+    plotWAC (data[, c("Time", "RSTT")], ylab=ylb)
+  }
+
+# Panel 3: VISB    
   if ("VISB" %in% VRPlot[[14]]) {
-    plotWAC (data[, c("Time", "VISB")])
+    plotWAC (data[, c("Time", "VISB")], ylab=ylb)
   }
-  op <- par (mar=c(5,4,0.5,1)+0.1)
+  
+# Panel 4: IRBC 
   if ("IRBC" %in% VRPlot[[14]] && "IRTC" %in% VRPlot[[14]]) {
-    plotWAC (data[, c("Time", "IRBC", "IRTC")], ylab="IRxC", legend.position='bottom')
-  } else {
-    plot (0,0, xlim=c(0,1), ylim=c(0,1), type='n', axes=FALSE, ann=FALSE)
-    text (0.5, 0.8, 'no IR radiation measurements')
-  }
+    plotWAC (data[, c("Time", "IRBC", "IRTC")], ylab=ylb)
+  } 
   AddFooter ()
 }
