f1ba011287f2ea11f6a5aaf4dc64f6056ab8c512
araudz <araudzens@gmail.com>
2018-07-17 10:51:38 -0600

    Added UHSAS/PCASP concentration and laser ref plots

--- PlotFunctions/RPlot15.R
+++ PlotFunctions/RPlot15.R
@@ -1,8 +1,10 @@
 ### plot 15: CN, FSSP, CDP, F300, CONCP, CONC1DC_LWOO
 RPlot15 <- function(data, Seq=NA) {
-  layout(matrix(1:2, ncol = 1), widths = 1, heights = c(5,6))
-  if (is.na(Seq) || (Seq == 1)) {
-    op <- par (mar=c(2,4,1,1)+0.1,oma=c(1.1,0,0,0))
+  layout(matrix(1:3, ncol = 1), widths = 1)
+  op <- par (mar=c(5,5,5,1),oma=c(2,2,2,1))
+  par(cex.lab=2, cex.main=2)
+
+#    if (is.na(Seq) || (Seq == 1)) {
     if (is.na(VRPlot[[15]][1])) {
       plot (0,0, xlim=c(0,1), ylim=c(0,1), type='n', axes=FALSE, ann=FALSE)
       text (0.5, 0.8, 'no particle or hydrometeor measurements', adj=0.5)
@@ -37,7 +39,7 @@ RPlot15 <- function(data, Seq=NA) {
         plotWAC (data[, c("Time", va)], 
           logxy='y', ylim=c(1,1.e5), 
           ylab=expression (paste ("CONCy [cm"^"-3"*"]")))
-        title ("1-min filter", cex.main=0.75)
+        title ("1-min filter applied")
       } else {
         plot (0,0, xlim=c(data$Time[1], data$Time[nrow(data)]), ylim=c(0,1), type='n', axes=FALSE, ann=FALSE)
         text (data$Time[1], 0.8, 'no particle measurements', adj=0.5)
@@ -47,7 +49,7 @@ RPlot15 <- function(data, Seq=NA) {
       tc <- data$Time[nrow(data) %/% 2]
       text (tc, 0.8, labels='no particle measurements')
     }
-    op <- par (mar=c(5,4,1,1)+0.1)
+#    op <- par (mar=c(5,4,1,1)+0.1)
     C <- VRPlot[[15]]
     C <- C[which("CONC" == substr(C, 1, 4) & "CONCU" != substr(C,1,5) & 
         "CONCN" != substr(C,1,5) & "CONCP" != substr(C,1,5))]
@@ -65,38 +67,38 @@ RPlot15 <- function(data, Seq=NA) {
     if (length(va2) > 0) {
       plotWAC (data[, c("Time", va2)],
              logxy='y', ylim=c(0.001,1e4), ylab=expression(paste("CONCy [cm"^"-3"*"]")))
-      title ("1-min filter", cex.main=0.75)
+      title ("1-min filter applied")
     } 
-    AddFooter ()
-    if (!is.na(Seq) && (Seq == 1)) {return ()}
-  }
-  op <- par (mar=c(5,4,1,1)+0.1)
-  nm6 <- names(data)[grepl("USHFLW_", names(data))]
+#    AddFooter ()
+#    if (!is.na(Seq) && (Seq == 1)) {return ()}
+#  }
+#  op <- par (mar=c(5,4,1,1)+0.1)
+  nm6 <- names(data)[grepl("UREF_", names(data))]
   if (length(nm6) == 0) {
       plot (0,0, xlim=c(0,1), ylim=c(0,1), type='n', axes=FALSE, ann=FALSE)
       text (0.5, 0.8, 'no housekeeping variables for the UHSAS')
       return()
   }
-  USHFLW <- data[, nm6]
-  nm7 <- names(data)[grepl("USMPFLW_", names(data))]
-  USMPFLW <- data[, nm7]
-  nm8 <- names(data)[grepl("UREF_", names(data))]
-  UREF <- data[, nm8]
-  nm9 <- names(data)[grepl("USCAT_", names(data))]
-  USCAT <- data[, nm9]
-  USHF <- USHFLW/10
-  plotWAC (data.frame(data$Time, USMPFLW, USHF),
-           ylab="flows", legend.position='topright',
-           ylim=c(0,2.5))
-  hline (0.82, 'blue'); hline (1, 'darkgreen'); hline(0.5, 'red'); hline (1.5, 'red')
-  legend ("topleft", legend=c("dashed red: limits for FCNC, XICNC, PFLWC", 
-                              "dashed blue-green: expected values for corresponding flows"), text.col=c('red', 'blue'), cex=0.55)
-  title ("USHF is USHFLW_RWOOU/10", cex.main=0.75)
-  op <- par (mar=c(5,4,1,1)+0.1)
-  plotWAC (data.frame (data$Time, UREF, USCAT), 
-           ylab="laser V", legend.position='topright', ylim=c(0,10))
-  hline (2.10, 'blue'); hline (1.90, 'darkgreen'); hline(6, 'red'); hline (9.95, 'red')
-  title ("dashed-blue: lower limit for UREF; dashed-green: upper limit for USCAT", cex.main=0.65)
+  UREF <- data[, nm6]
+  nm7 <- names(data)[grepl("PREF_", names(data))]
+  PREF <- data[, nm7]
+  # nm8 <- names(data)[grepl("UREF_", names(data))]
+  # UREF <- data[, nm8]
+  # nm9 <- names(data)[grepl("USCAT_", names(data))]
+  # USCAT <- data[, nm9]
+  # USHF <- USHFLW/10
+  plotWAC (data.frame(data$Time, UREF, PREF),
+           ylab="voltages", ylim=c(0,10))
+  hline(2, lty=2,lwd=2, 'blue'); hline(6, lty=2, lwd=2 ,'green')
+  # hline (0.82, 'blue'); hline (1, 'darkgreen'); hline(0.5, 'red'); hline (1.5, 'red')
+  # legend ("topleft", legend=c("dashed red: limits for FCNC, XICNC, PFLWC", 
+  #                             "dashed blue-green: expected values for corresponding flows"), text.col=c('red', 'blue'), cex=0.55)
+ title ("Laser Reference Values UHSAS, PCASP")
+  # op <- par (mar=c(5,4,1,1)+0.1)
+  # plotWAC (data.frame (data$Time, UREF, USCAT), 
+  #          ylab="laser V", legend.position='topright', ylim=c(0,10))
+  # hline (2.10, 'blue'); hline (1.90, 'darkgreen'); hline(6, 'red'); hline (9.95, 'red')
+  # title ("dashed-blue: lower limit for UREF; dashed-green: upper limit for USCAT", cex.main=0.65)
   AddFooter ()
 }
 
