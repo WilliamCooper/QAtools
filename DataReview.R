6a523a54190ac34360b30f4c437d33a6757b76f3
Taylor Thomas <taylort@datavis.eol.ucar.edu>
2021-08-02 16:28:27 -0600

    fix for unusual project name in MethaneAIR21, relies on flight arg

--- DataReview.R
+++ DataReview.R
@@ -146,7 +146,6 @@ for (Flight in Flt) {
       print (sprintf ('output file for flight %s already exists; skipping (purge to replace it)', spf))
     } else {
       
-
       netCDFfile <- NULL
       CCDP <- NULL
       CFSSP <- NULL
@@ -159,9 +158,14 @@ for (Flight in Flt) {
       } else {
 ##          fnumber <- as.numeric (sub('[a-zA-Z]*([0-9]*).nc', '\\1', Flight))
 ##          ftype <- sub('[A-Za-z]*(.f)[0-9]*.nc', '\\1', Flight)
-## 210712, BBS: the above was not handling hyphens in project names (e.g. WCR-TEST or ASPIRE-TEST), added hyphens:
-          fnumber <- as.numeric (sub('[a-zA-Z-]*([0-9]*).nc', '\\1', Flight))
-          ftype <- sub('[A-Za-z-]*(.f)[0-9]*.nc', '\\1', Flight)
+# 210712, BBS: the above was not handling hyphens in project names (e.g. WCR-TEST or ASPIRE-TEST), added hyphens:
+##            fnumber <- as.numeric (sub('[A-Za-z-]*([0-9]*).nc', '\\1', Flight))
+##            ftype <- sub('[A-Za-z-]*(.f)[0-9]*.nc', '\\1', Flight)
+# 210729, BBS: now causing problems for MethaneAIR21 because of the numbers in the project name. . . 
+# 210802, TMT: this fix relies on a flight argument, but it will work for MethaneAIR
+        flight_info <- strsplit(FlightX, 0)
+        ftype <- flight_info[[1]][1]
+        fnumber <- as.numeric(flight_info[[1]][2])
       }
       ## next statement needs to be inside "ALL" loop, in case available variables change
       VRPlot <- loadVRPlot (Project, FALSE, fnumber, psq)  ## get VRPlot list for this project
