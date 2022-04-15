31e4ce44e66982fa6a75b7ba43cc2994518c84c2
WilliamCooper <cooperw@ucar.edu>
2022-04-15 15:29:55 -0600

    Added 2DS to concentration plot in Configuration.R. (Are units right: cm^-3?) Also, modified title of WI plot to list means for all WI variables.

--- PlotFunctions/RPlot9.R
+++ PlotFunctions/RPlot9.R
@@ -108,7 +108,11 @@ RPlot9 <- function (data, Seq = NA, panl = 1) {
       cex.lab = 1.5,
       ylim = YLMF (3, range (as.matrix (data[, WI]), finite = TRUE))
     )
-    if (length(WI) == 2) {
+    if (length(WI) >= 3) {
+      title (sprintf (
+        "averages: %s= %.02f, %s = %0.02f %s=%.02f", WI[1], mean(data[, WI[1]], na.rm = TRUE),
+        WI[2], mean(data[, WI[2]], na.rm = TRUE), WI[3], mean(data[, WI[3]], na.rm = TRUE)), cex.main = 1.2)  
+    } else if (length(WI) == 2) {
       title (sprintf (
         "averages: %s= %.02f, %s = %0.02f", WI[1], mean(data[, WI[1]], na.rm = TRUE),
         WI[2], mean(data[, WI[2]], na.rm = TRUE)), cex.main = 1.2)  
